f40bd75fb367ed384203adaa1fd89b387bf1b7a0
Fauns0 <luizhhuu@gmail.com>
2024-11-26 22:32:51 -0400

    Alterado o horario de funciomaneto.

--- src/components/HorarioFuncionamento.js
+++ src/components/HorarioFuncionamento.js
@@ -7,7 +7,7 @@ function HorarioFuncionamento() {
     <section id="funcionamento" className="HorarioFuncionamento container">
       <div className="HorarioFuncionamento-content">
         <h2>Horário de Funcionamento</h2>
-        <p>Atendemos de segunda a sexta-feira, das 8h às 18h.</p>
+        <p>Atendemos de segunda a sexta-feira, das das 8h às 11h/13h às 18h</p>
         <p>
           Estamos à disposição durante a semana para oferecer serviços rápidos e eficientes. Para
           agendamentos ou dúvidas, entre em contato conosco por telefone ou faça-nos uma visita.
